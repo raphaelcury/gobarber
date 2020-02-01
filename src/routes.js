75d61c1611fc72f2ad227645e9b0089b4db0e78e
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-02-01 17:38:15 -0300

    Estrutura pastas, config DB, primeira migration.

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/',(req, res) => {
-  return res.json({ message: 'Hello Raphael!!!' });
-});
+routes.get('/', (req, res) => res.json({ message: 'Hello Raphael!!!' }));
 
 export default routes;
