b15e97af3ffc61c6facb0910ee4c9947280df7e5
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-02-17 11:24:52 -0300

    File upload on /files route

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -6,6 +8,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // Rotas sem autenticacao
 routes.post('/sessions', SessionController.store);
@@ -14,5 +17,8 @@ routes.post('/users', UserController.store);
 // A partir daqui o middleware de autenticação se aplica
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
