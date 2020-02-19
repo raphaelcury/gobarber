a858f8f6739d7c649cf568eca46c94f5a0e3574c
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-02-19 14:55:51 -0300

    Adding MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -10,14 +11,28 @@ const models = [User, File, Appointment];
 
 class Database {
   constructor() {
-    this.init();
+    this.initSql();
+    this.initMongo();
   }
 
-  init() {
-    this.connection = new Sequelize(databaseConfig);
+  initSql() {
+    this.sqlConnection = new Sequelize(databaseConfig);
     models
-      .map(model => model.init(this.connection))
-      .map(model => model.associate && model.associate(this.connection.models));
+      .map(model => model.init(this.sqlConnection))
+      .map(
+        model => model.associate && model.associate(this.sqlConnection.models)
+      );
+  }
+
+  initMongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
