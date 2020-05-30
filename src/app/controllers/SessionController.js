16a277ad29d4b7883cecaf020247e23b1fafc3f4
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-30 15:18:30 -0300

    Session creation returning avatar data.

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 
 import User from '../models/User';
+import File from '../models/File';
 import authConfig from '../../config/auth';
 
 class SessionController {
@@ -18,7 +19,14 @@ class SessionController {
     }
 
     const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: {
+        model: File,
+        as: 'avatar',
+        attributes: ['id', 'path', 'url'],
+      },
+    });
     if (!user) {
       // User not found
       return res.status(401).json({ error: 'User not found' }); // Unauthorized
@@ -27,12 +35,17 @@ class SessionController {
       // Password does not match
       return res.status(401).json({ error: 'Wrong password' }); // Unauthorized
     }
-    const { id, name } = user;
+    const { id, name, avatar } = user;
     return res.json({
       user: {
         id,
         name,
         email,
+        avatar: {
+          id: avatar.id,
+          path: avatar.path,
+          url: avatar.url,
+        },
       },
       // payload = id
       token: jwt.sign({ id }, authConfig.secret, {
